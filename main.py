023493f5f8cd87f58c89bee28f9afea747f9b9ee
MichaelFine2022 <mf9947967@gmail.com>
2024-11-22 10:50:48 -0800

    merged files and made basic login/signin navigation

--- main.py
+++ main.py
@@ -1,17 +1,43 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify
-from flask import Flask_SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 
-from config import app
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'  
+db = SQLAlchemy(app)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
 
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password 
+
+    def __repr__(self):
+        return f'<User {self.username}>'
+    
+    def to_json(self):
+        return{
+            "id":self.id,
+            "username":self.username,
+            "password":self.password,}
+
+#base login page
 @app.route('/')
 def index():
     return render_template('login.html')
 
-@app.route("/signIn",methods=["POST"])
-def create_user():
-    username = request.json.get("username")
-    if not username:
-        return jsonify({"message":"Failed"}),400
+#redirection to signin page
+@app.route("/tosignin",methods=["GET"])
+def toSignIn():
+    return render_template('signin.html')
+
+@app.route("/tologin",methods=["GET"])
+def toLogin():
+    return render_template('login.html')
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
