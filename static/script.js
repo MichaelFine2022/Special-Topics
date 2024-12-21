8ff7dd922d763e10e7d9465702cc9cdeb2bf6cbd
MichaelFine2022 <mf9947967@gmail.com>
2024-12-21 15:42:07 -0800

    Added dark mode and split up web page CSS

--- static/script.js
+++ static/script.js
@@ -1,26 +1,43 @@
-const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
-
-function switchTheme(e) {
-    if (e.target.checked) {
-        document.documentElement.setAttribute('data-theme', 'dark');
-        localStorage.setItem('theme', 'dark');
-    }
-    else {
-        document.documentElement.setAttribute('data-theme', 'light');
-        localStorage.setItem('theme', 'light'); 
-    }    
+function calculate({ localStorageTheme, systemSettingDark }) {
+    return localStorageTheme || (systemSettingDark.matches ? "dark" : "light");
 }
-
-toggleSwitch.addEventListener('change', switchTheme, false);
-
-addEventListener("DOMContentLoaded", (event) => {
-    const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
-
-    if (currentTheme) {
-        document.documentElement.setAttribute('data-theme', currentTheme);
-
-        if (currentTheme === 'dark') {
-            toggleSwitch.checked = true;
-        }
+  
+function updateButton({ buttonEl, isDark }) {
+    const newCta = isDark ? "Change to light theme" : "Change to dark theme";
+    buttonEl.setAttribute("aria-label", newCta);
+    buttonEl.innerText = newCta;
+}
+  
+function updateTheme({ theme }) {
+    document.documentElement.setAttribute("data-theme", theme);
+}
+  
+const button = document.querySelector("[data-theme-toggle]");
+const localStorageTheme = localStorage.getItem("theme");
+const systemSettingDark = window.matchMedia("(prefers-color-scheme: dark)");
+  
+let currentThemeSetting = calculate({ localStorageTheme, systemSettingDark });
+  
+updateButton({ buttonEl: button, isDark: currentThemeSetting === "dark" });
+updateTheme({ theme: currentThemeSetting });
+  
+button.addEventListener("click", () => {
+    const newTheme = currentThemeSetting === "dark" ? "light" : "dark";
+  
+    localStorage.setItem("theme", newTheme);
+    updateButton({ buttonEl: button, isDark: newTheme === "dark" });
+    updateTheme({ theme: newTheme });
+  
+    currentThemeSetting = newTheme;
+});
+  
+  // Optional: Add listener for system theme changes
+systemSettingDark.addEventListener("change", (event) => {
+    if (!localStorage.getItem("theme")) {
+      const newSystemTheme = event.matches ? "dark" : "light";
+      updateTheme({ theme: newSystemTheme });
+      updateButton({ buttonEl: button, isDark: newSystemTheme === "dark" });
+      currentThemeSetting = newSystemTheme;
     }
 });
+  
